07218de98155947c92133cd164d0c87d46e29013
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2024-02-10 19:19:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
 import streamlit as st
-import os
 import numpy as np
 from sklearn.cluster import KMeans
 import joblib  # Import joblib
 from PIL import Image
-from zipfile import ZipFile
 import matplotlib.pyplot as plt
-
-# Function to crawl images from a directory
-def crawl_images(path):
-    images = []
-    imageNames = []
-    with os.scandir(path) as files:
-        for file in files:
-            with os.scandir(file) as rooms:
-                for imgs in rooms:
-                    if imgs.name.endswith('.jpg'):
-                        file_path = os.path.join(path, file.name, imgs.name)
-                        imageNames.append(file_path)
-                        img = Image.open(file_path)
-                        img = img.resize((32, 32))
-                        img = np.array(img)
-                        images.append(img)
-    return np.array(images), np.array(imageNames)
 
 # Function to process images from a zip file
 def process_zip_file(zip_file):
     images = []
     imageNames = []
-    with ZipFile(zip_file) as z:
-        for file_info in z.infolist():
-            if file_info.filename.endswith('.jpg'):
-                with z.open(file_info.filename) as file:
-                    img = Image.open(file)
-                    img = img.resize((32, 32))
-                    img = np.array(img)
-                    images.append(img)
-                    imageNames.append(file_info.filename)
+    with st.spinner("Processing ZIP file..."):
+        with st.beta_expander("See files in ZIP"):
+            with st.echo():
+                with zipfile.ZipFile(zip_file) as z:
+                    for file_info in z.infolist():
+                        if file_info.filename.endswith('.jpg'):
+                            imageNames.append(file_info.filename)
+                            img = Image.open(z.open(file_info.filename))
+                            img = img.resize((32, 32))
+                            img = np.array(img)
+                            images.append(img)
+                            st.image(img, caption=file_info.filename, use_column_width=True)
+
     return np.array(images), np.array(imageNames)
 
 # Function that lets you view a cluster (based on identifier)
@@ -62,20 +47,11 @@ def main():
     groups = None
 
     # Sidebar: Input method selection
-    input_method = st.sidebar.radio("Select Input Method:", ("Web Crawl", "Upload ZIP"))
-
-    if input_method == "Web Crawl":
-        st.sidebar.header("Web Crawl Settings")
-        crawl_path = st.sidebar.text_input("Enter the directory path for web crawl:")
-        if st.sidebar.button("Crawl Images"):
-            train_images, train_labels = crawl_images(crawl_path)
-
-    elif input_method == "Upload ZIP":
-        st.sidebar.header("ZIP Upload Settings")
-        uploaded_zip = st.sidebar.file_uploader("Upload ZIP file", type="zip")
-        if uploaded_zip is not None:
-            if st.sidebar.button("Process ZIP"):
-                train_images, train_labels = process_zip_file(uploaded_zip)
+    st.sidebar.header("ZIP Upload Settings")
+    uploaded_zip = st.sidebar.file_uploader("Upload ZIP file", type="zip")
+    if uploaded_zip is not None:
+        if st.sidebar.button("Process ZIP"):
+            train_images, train_labels = process_zip_file(uploaded_zip)
 
     # Train and Cluster button in the main section
     if train_images is not None:
